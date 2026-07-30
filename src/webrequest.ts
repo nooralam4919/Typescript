edcee857411e35da827d61c3a0b46107a48ed8fc
Noor Alam <alamn5063@gmail.com>
2026-07-31 00:46:35 +0530

    webreqest

---
+++ src/webrequest.ts
@@ -0,0 +1,27 @@
+// import axios, { AxiosResponse } from "axios";
+
+// interface Todo {
+//     userId: number,
+//     id: number,
+//     tittle: string,
+//     completed: boolean
+// }
+
+// const fetchData = async () => {
+//     try{
+//         const response: AxiosResponse<Todo> = await axios.get(
+//             //api call
+//         )
+//         // console.log("Todo", response.data) give all suggestion
+//     }
+//     catch(error: any){
+//         // console.log(error.message)
+//         if(axios.isAxiosError(error))
+//         {
+//             console.log("axios eroro", error.message)
+//             if(error.response){
+//                 console.log(error.response.status)
+//             }
+//         }
+//     }
+// }
